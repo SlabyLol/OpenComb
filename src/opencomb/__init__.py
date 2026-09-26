d911bdd719307418256cf145a1e3aa7595527d69
DarkFox <darkfox.tobias@outlook.com>
2026-09-26 09:37:10 +0200

    chore: bump __version__ to 0.19.2

--- src/opencomb/__init__.py
+++ src/opencomb/__init__.py
@@ -3,7 +3,7 @@ OpenComb – Everything developers need.
 
 >>> import opencomb
 >>> opencomb.__version__
-'0.19.1'
+'0.19.2'
 """
 
 from __future__ import annotations
@@ -50,7 +50,7 @@ from opencomb.api import (
     search, todos, stats, doctor,
 )
 
-__version__ = "0.19.1"
+__version__ = "0.19.2"
 
 __all__ = [
     "__version__",
